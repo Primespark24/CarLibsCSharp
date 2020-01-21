53a7dd6ee93ef56eb19cbc153591c6a5a539f9a9
Brycen Martin <bmartin22@my.whitworth.edu>
2020-01-21 12:36:37 -0800

    addded the classes and usage with assemblies

---
+++ ClassLibrary1/ClassLibrary1/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLib
+{
+    public enum EngineState { EngineOn, EngineOff}
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int maxspeed { get; set; }
+        protected EngineState EngState = EngineState.EngineOn;
+        public EngineState EngineState => EngState;
+        public abstract void Turbo();
+        public Car() { }
+        public Car(string name, int maxSpd, int currSpd)
+        {
+            PetName = name; maxspeed = maxSpd; CurrentSpeed = currSpd; 
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/DerivedCars.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLib
+{
+    class SportsCar : Car
+    {
+        public SportsCar() { }
+        public SportsCar(string name, int maxSpd, int currSpd) : base (name, maxSpd, currSpd) { }
+        public override void Turbo()
+        {
+            Console.WriteLine("Ramming Speed!");
+        }
+    }
+
+    class MiniVan : Car
+    {
+        public MiniVan() { }
+        public MiniVan(string name, int maxSpd, int currSpd) : base(name, maxSpd, currSpd) { }
+        public override void Turbo()
+        {
+            EngState = EngineState.EngineOff;
+            Console.WriteLine("Engine Exploded!");
+        }
+    }
+}
